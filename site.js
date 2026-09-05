9adc5d258aaf41983364b0f0d784436ef006ff1c
LHBUSA <sales@localhomebuyersusa.com>
2026-09-05 06:08:28 -0500

    Load deep New Zealand property graph docs

--- site.js
+++ site.js
@@ -1,4 +1,6 @@
 (()=>{
   const load=(src,done)=>{const s=document.createElement('script');s.src=src;s.async=false;if(done)s.onload=done;document.head.appendChild(s)};
-  load('/site.core.js',()=>load('/product-demo.js'));
+  load('/site.core.js',()=>load('/product-demo.js',()=>{
+    if(location.pathname.startsWith('/docs'))load('/docs-depth.js');
+  }));
 })();
